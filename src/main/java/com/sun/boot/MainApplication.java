ca94772509506496dc5ce46a3b79d0411eccc887
sunzibin <sunzibin@sunline.cn>
2021-05-23 23:43:59 +0800

    新添加功能

---
+++ src/main/java/com/sun/boot/MainApplication.java
@@ -0,0 +1,22 @@
+package com.sun.boot;
+
+import com.sun.boot.bean.Human;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication(scanBasePackages = "com.sun.boot")
+public class MainApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+
+        String[] names = run.getBeanDefinitionNames();
+
+//        for(String s :names){
+//            System.out.println(s);
+//        }
+
+        Human human01 = run.getBean("human01", Human.class);
+        System.out.println(human01.toString());
+    }
+}
