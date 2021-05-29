d7103ed5215ff68665789ef2e30cde8d07e88bb1
sunzibin3 <84666002+sunzibin3@users.noreply.github.com>
2021-05-29 19:06:34 +0800

    Update GitTest.java

--- src/main/java/com/sun/boot/GitTest.java
+++ src/main/java/com/sun/boot/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("cvvvvvv");
         System.out.println("测试idea Push");
+        System.out.println("测试idea pull");
     }
 }
