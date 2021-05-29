132822d4778d4a783becc98f8d43bef40420f1d1
sunzibin <sunzibin@sunline.cn>
2021-05-29 18:51:23 +0800

    master分支测试合并冲突

--- src/main/java/com/sun/boot/GitTest.java
+++ src/main/java/com/sun/boot/GitTest.java
@@ -3,5 +3,6 @@ package com.sun.boot;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("cvvvvvv");
+        System.out.println("测试idea Push");
     }
 }
